20c7d1cab592bbc5fbaeeff15887b45391457d2c
Sara Bsharat <sarabsharat.sb@gmail.com>
2026-05-02 21:09:53 +0300

    Update base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/writing-club-scoreboards/', // <-- Replace with your exact GitHub repo name!
+    base: '/writing-club-scoreboards/',
 })
